951fe7c9e3cfbe1dccaf9ac5e62cc17e9c4cc174
Tony Allevato <allevato@vt.edu>
2012-11-11 13:09:41 -0500

    Converted OvalShape to physics.

--- src/sofia/graphics/OvalShape.java
+++ src/sofia/graphics/OvalShape.java
@@ -1,5 +1,12 @@
 package sofia.graphics;
 
+import org.jbox2d.collision.shapes.CircleShape;
+import org.jbox2d.collision.shapes.PolygonShape;
+import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.Body;
+
+import sofia.graphics.internal.Box2DUtils;
+
 import android.graphics.Paint;
 import android.graphics.Canvas;
 import android.graphics.PointF;
@@ -14,6 +21,15 @@ import android.graphics.RectF;
  */
 public class OvalShape extends FillableShape
 {
+    //~ Fields ................................................................
+
+    private static final int OVAL_VERTEX_COUNT = 24;
+
+    private PointF center;
+    private float xRadius;
+    private float yRadius;
+
+
     //~ Constructors ..........................................................
 
     // ----------------------------------------------------------
@@ -22,7 +38,7 @@ public class OvalShape extends FillableShape
      */
     public OvalShape()
     {
-        super();
+        this(new PointF(0, 0), 0, 0);
     }
 
 
@@ -32,7 +48,8 @@ public class OvalShape extends FillableShape
      */
     public OvalShape(RectF bounds)
     {
-        setBounds(bounds);
+        this(new PointF(bounds.centerX(), bounds.centerY()),
+                bounds.width() / 2, bounds.height() / 2);
     }
 
 
@@ -46,7 +63,7 @@ public class OvalShape extends FillableShape
      */
     public OvalShape(PointF center, float radius)
     {
-        this(Anchor.CENTER.anchoredAt(center).sized(radius * 2, radius * 2));
+        this(center, radius, radius);
     }
 
 
@@ -61,7 +78,7 @@ public class OvalShape extends FillableShape
      */
     public OvalShape(float x, float y, float radius)
     {
-        this(Anchor.CENTER.anchoredAt(x, y).sized(radius * 2, radius * 2));
+        this(new PointF(x, y), radius);
     }
 
 
@@ -77,8 +94,10 @@ public class OvalShape extends FillableShape
     public OvalShape(PointF center, float horizontalRadius,
             float verticalRadius)
     {
-        this(Anchor.CENTER.anchoredAt(center).sized(
-                horizontalRadius * 2, verticalRadius * 2));
+        this.center = center;
+        this.xRadius = horizontalRadius;
+        this.yRadius = verticalRadius;
+        updateTransform(center.x, center.y);
     }
 
 
@@ -95,8 +114,7 @@ public class OvalShape extends FillableShape
     public OvalShape(float x, float y, float horizontalRadius,
             float verticalRadius)
     {
-        this(Anchor.CENTER.anchoredAt(x, y).sized(
-                horizontalRadius * 2, verticalRadius * 2));
+        this(new PointF(x, y), horizontalRadius, verticalRadius);
     }
 
 
@@ -124,23 +142,53 @@ public class OvalShape extends FillableShape
 
     // ----------------------------------------------------------
     @Override
+    public RectF getBounds()
+    {
+        // If the body has been created, update the center point using the
+        // body's current position.
+
+        Body b2Body = getB2Body();
+        if (b2Body != null)
+        {
+            Box2DUtils.vec2ToPointF(b2Body.getPosition(), center);
+        }
+
+        return new RectF(center.x - xRadius, center.y - yRadius,
+                center.x + xRadius, center.y + yRadius);
+    }
+
+
+    // ----------------------------------------------------------
+    @Override
+    public void setBounds(RectF newBounds)
+    {
+        updateTransform(newBounds.centerX(), newBounds.centerY());
+        xRadius = newBounds.width() / 2;
+        yRadius = newBounds.height() / 2;
+
+        recreateFixtures();
+        conditionallyRepaint();
+    }
+
+
+    // ----------------------------------------------------------
+    @Override
     public boolean contains(float x, float y)
     {
-/*        float[] point = inverseTransformPoint(x, y);
+        float[] point = inverseTransformPoint(x, y);
 
-        double rx = getWidth() / 2;
-        double ry = getHeight() / 2;
+        double rx = xRadius;
+        double ry = yRadius;
 
         if (rx == 0 || ry == 0)
         {
             return false;
         }
 
-        double dx = point[0] - (getX() + rx);
-        double dy = point[1] - (getY() + ry);
+        double dx = point[0] - center.x;
+        double dy = point[1] - center.y;
 
-        return (dx * dx) / (rx * rx) + (dy * dy) / (ry * ry) <= 1.0;*/
-        return true;
+        return (dx * dx) / (rx * rx) + (dy * dy) / (ry * ry) <= 1.0;
     }
 
 
@@ -165,6 +213,71 @@ public class OvalShape extends FillableShape
     @Override
     protected void createFixtures()
     {
-        // TODO Auto-generated method stub
+        if (Math.abs(xRadius - yRadius) < 0.001)
+        {
+            // It's a circle, so use a CircleShape.
+            CircleShape circle = new CircleShape();
+            circle.m_radius = xRadius;
+            addFixtureForShape(circle);
+        }
+        else
+        {
+            int vertex = 0;
+
+            Vec2[] vertices = new Vec2[8];
+
+            // This algorithm splits the oval into polygonal slices that
+            // each have 8 vertices (the center of the oval and 7 vertices
+            // around the circumference), except for the final slice, which
+            // only contains enough of the remaining vertices to complete
+            // the final slice.
+
+            while (vertex < OVAL_VERTEX_COUNT)
+            {
+                vertices[0] = new Vec2(0, 0);
+                int verticesUsed = 1;
+                int startVertex = vertex;
+
+                for (int i = 0; i < 7 &&
+                        startVertex + i <= OVAL_VERTEX_COUNT;
+                        i++, vertex++)
+                {
+                    float fraction = (float) vertex / OVAL_VERTEX_COUNT;
+                    vertices[verticesUsed++] =
+                            pointOnEllipse(xRadius, yRadius, fraction);
+                }
+
+                PolygonShape polygon = new PolygonShape();
+                polygon.set(vertices, verticesUsed);
+                addFixtureForShape(polygon);
+
+                if (vertex != OVAL_VERTEX_COUNT)
+                {
+                    // Roll back one so that the slices touch each other.
+                    vertex--;
+                }
+            }
+        }
+    }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Gets the location of a vertex on the circumference of an ellipse.
+     *
+     * @param xrad the horizontal radius of the ellipse
+     * @param yrad the vertical radius of the ellipse
+     * @param fraction a value from 0 to 1 indicating the location along the
+     *     circumference of the point that should be computed
+     * @return a {@code Vec2} object with the (x, y) coordinates of the point
+     *     on the ellipse
+     */
+    private static Vec2 pointOnEllipse(float xrad, float yrad, float fraction)
+    {
+        double angle = fraction * 2 * Math.PI;
+        float x = (float) (xrad * Math.cos(angle));
+        float y = (float) (yrad * Math.sin(angle));
+
+        return new Vec2(x, y);
     }
 }
