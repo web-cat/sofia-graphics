5cd01fe748402b0441c8635b2b97e4f79921adf3
Tony Allevato <allevato@vt.edu>
2013-03-10 13:31:18 -0400

    Removed unused import.

--- src/sofia/graphics/Polygon.java
+++ src/sofia/graphics/Polygon.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-import org.jbox2d.collision.shapes.PolygonShape;
-
 import android.graphics.PointF;
 
 //-------------------------------------------------------------------------
